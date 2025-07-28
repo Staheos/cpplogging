2d149c2d927400c28252ab4df148e6de62116306
Staheos <staslewy@gmail.com>
2025-07-28 17:42:38 +0200

    More file and cout options.

--- include/cpplogging/Logger.hpp
+++ include/cpplogging/Logger.hpp
@@ -1,7 +1,11 @@
 #ifndef WSTL_LOGGING_LOGGER
 #define WSTL_LOGGING_LOGGER
 
+
+#ifndef COMPILATION_LOG_LEVEL
 #define COMPILATION_LOG_LEVEL wstl::log::LogLevel::debug
+#endif
+
 
 #include <iostream>
 #include <string>
@@ -19,7 +23,7 @@
 
 
 #define DATE() \
-    []() { \
+	[]() { \
 		std::chrono::time_point<std::chrono::system_clock> now = std::chrono::system_clock::now(); \
 		int64_t timestamp = now.time_since_epoch().count(); \
 		std::time_t currentTime = std::chrono::system_clock::to_time_t(now); \
@@ -29,196 +33,196 @@
 		ss_date << std::put_time(&utcTime, "%Y-%m-%d_%H:%M:%S") << ':' << std::setfill('0') << std::setw(3) << std::chrono::duration_cast<std::chrono::milliseconds>(now.time_since_epoch()).count() % 1000; \
 		return ss_date.str(); }()
 
+
 namespace wstl
 {
-    namespace log
-    {
+namespace log
+{
 
-        class Logger
-        {
-            std::queue<std::string> _queue;
-            std::mutex _mutex;
-            std::condition_variable _cv;
-            std::atomic<bool> _running;
-            std::thread _workerThread;
-            std::string _filename = "log.txt";
+class Logger
+{
+	std::queue<std::string> _queue;
+	
+	std::condition_variable _cv;
+	std::atomic<bool> _running;
+	std::thread _workerThread;
 
-        private:
-            /// Constructor shall be private in singletons, initialize the worker thread and open the default log file.
-            Logger() : _running(true)
-            {
-                // Launch the worker thread
-                _workerThread = std::thread(&Logger::workerLoop, this);
-            }
+	std::atomic<bool> _logStdio = true;
+	std::string _filepath = "";
 
-        public:
-            static void log(const std::string &message, const wstl::log::LogLevel logLevel = wstl::log::LogLevel::info)
-            {
-                Logger::instance().Log(wstl::log::color_string(message, wstl::log::LogLevelColor(logLevel)));
-            }
+	std::mutex _mutex;
+	std::mutex _filepathMutex;
 
-            template <typename... Args>
-            static void logf(const std::string &format, wstl::log::LogLevel logLevel, Args... args)
-            {
-                Logger::log(fmt::format(format, args...), logLevel);
-            }
+private:
+	/// Constructor shall be private in singletons, initialize the worker thread and open the default log file.
+	Logger() : _running(true)
+	{
+		// Launch the worker thread
+		_workerThread = std::thread(&Logger::workerLoop, this);
+	}
 
-            template <wstl::log::LogLevel logLevel, typename... Args>
-            constexpr static void logfl(const std::string &format, Args... args)
-            {
-                if constexpr (COMPILATION_LOG_LEVEL <= logLevel)
-                {
-                    Logger::logf("[" + DATE() + "] " + format, logLevel, args...);
-                }
-            }
+public:
+	static void log(const std::string& message, const wstl::log::LogLevel logLevel = wstl::log::LogLevel::info)
+	{
+		Logger::instance().Log(wstl::log::color_string(message, wstl::log::LogLevelColor(logLevel)));
+	}
 
-            /*template <typename ...Args>
-            static void logFatal(const std::string format, Args ...args)
-            {
-                Logger::logf(format, wstl::log::LogLevel::fatal, args...);
-            }
+	template <typename... Args>
+	static void logf(const std::string& format, wstl::log::LogLevel logLevel, Args... args)
+	{
+		Logger::log(fmt::format(format, args...), logLevel);
+	}
 
-            template <typename ...Args>
-            static void logError(const std::string format, Args ...args)
-            {
-                Logger::logf(format, wstl::log::LogLevel::error, args...);
-            }
+	template <wstl::log::LogLevel logLevel, typename... Args>
+	constexpr static void logfl(const std::string& format, Args... args)
+	{
+		if constexpr (COMPILATION_LOG_LEVEL <= logLevel)
+		{
+			Logger::logf("[" + DATE() + "] " + format, logLevel, args...);
+		}
+	}
 
-            template <typename ...Args>
-            static void logWarning(const std::string format, Args ...args)
-            {
-                logger::logf(format, wstl::log::LogLevel::warning, args...);
-            }
+	static Logger& instance()
+	{
+		static Logger instance;
+		return instance;
+	}
 
-            template <typename ...Args>
-            static void logInfo(const std::string format, Args ...args)
-            {
-                logger::logf(format, wstl::log::LogLevel::info, args...);
-            }
+	void Log(const std::string& message)
+	{
+		{
+			std::lock_guard<std::mutex> lock(_mutex);
+			_queue.push(message);
+		}
+		_cv.notify_one();
+	}
 
-            template <typename ...Args>
-            static void logDebug(const std::string format, Args ...args)
-            {
-                logger::logf(format, wstl::log::LogLevel::debug, args...);
-            }
+	/// @brief Thread-safe mechanism to enable/disable console output.
+	void setConsoleOutput(const bool enabled)
+	{
+		this->_logStdio.store(enabled);
+	}
 
-            template <typename ...Args>
-            static void logTrace(const std::string format, Args ...args)
-            {
-                logger::logf(format, wstl::log::LogLevel::trace, args...);
-            }
+	/// @brief Thread-safe mechanism to set output file, use "" for no file log.
+	void setOutputFile(const std::string& filepath)
+	{
+		std::lock_guard<std::mutex> lock(_mutex);
+		_filepath = filepath;
+	}
 
-            template <typename ...Args>
-            static void logOk(const std::string format, Args ...args)
-            {
-                logger::logf(format, wstl::log::LogLevel::ok, args...);
-            }
+	// Make sure to properly stop the worker thread when shutting down
+	void stop()
+	{
+		bool expected = true;
+		if (_running.compare_exchange_strong(expected, false))
+		{
+			// If we successfully changed it from true to false
+			_cv.notify_one(); // Wake up the worker thread
+			if (_workerThread.joinable())
+			{
+				_workerThread.join();
+			}
+		}
+	}
 
-            template <typename ...Args>
-            static void logImportant(const std::string format, Args ...args)
-            {
-                logger::logf(format, wstl::log::LogLevel::important, args...);
-            }*/
+private:
+	~Logger()
+	{
+		// Safely stop worker thread
+		stop();
+	}
 
-            // Singleton access if desired, or just create an instance somewhere
-            static Logger &instance()
-            {
-                static Logger instance;
-                return instance;
-            }
+public:
+	/// Not copyable
+	Logger(const Logger&) = delete;
 
-            // Public interface for log
-            void Log(const std::string &message)
-            {
-                {
-                    std::lock_guard<std::mutex> lock(_mutex);
-                    _queue.push(message);
-                }
-                _cv.notify_one();
-            }
+	/// Not assignable
+	Logger& operator=(const Logger&) = delete;
 
-            // Optional: Provide some thread-safe mechanism to set output file, etc.
-            void setOutputFile(const std::string &filename)
-            {
-                std::lock_guard<std::mutex> lock(_mutex);
-                _filename = filename;
-            }
+private:
+	/// Worker thread loop to handle IO operations on console or files
+	void workerLoop()
+	{
+		// // Close the current file if open
+		// if (_outputStream.is_open()) {
+		//     _outputStream.close();
+		// }
+		// _outputStream.open(filename, std::ios::app);
 
-            // Make sure to properly stop the worker thread when shutting down
-            void stop()
-            {
-                bool expected = true;
-                if (_running.compare_exchange_strong(expected, false))
-                {
-                    // If we successfully changed it from true to false
-                    _cv.notify_one(); // Wake up the worker thread
-                    if (_workerThread.joinable())
-                    {
-                        _workerThread.join();
-                    }
-                }
-            }
+		while (_running)
+		{
+			std::unique_lock<std::mutex> lock(_mutex);
+			_cv.wait(lock, [this]
+			{
+				// Wake up if new data is available or if we are shutting down
+				return !_queue.empty() || !_running;
+			});
 
-        private:
-            ~Logger()
-            {
-                // Safely stop worker thread
-                stop();
-            }
+			const bool logStdio = this->_logStdio.load();
+			const std::string filepath = _filepath;
+			std::ofstream _fileStream;
 
-        public:
-            /// Not copyable
-            Logger(const Logger &) = delete;
+			if (!filepath.empty())
+			{
+				_fileStream = std::ofstream(filepath, std::ios::app);
+			}
 
-            /// Not assignable
-            Logger &operator=(const Logger &) = delete;
+			while (!_queue.empty())
+			{
+				const std::string& msg = _queue.front();
 
-        private:
-            /// Worker thread loop to handle IO operations on console or files
-            void workerLoop()
-            {
-                std::ofstream _outputStream; // Log file handle
-                _outputStream = std::ofstream(_filename, std::ios::app);
+				if (logStdio)
+				{
+					std::cout << msg << '\n';
+				}
 
-                // // Close the current file if open
-                // if (_outputStream.is_open()) {
-                //     _outputStream.close();
-                // }
-                // _outputStream.open(filename, std::ios::app);
+				if (!filepath.empty())
+				{
+					_fileStream << msg << '\n';
+				}
 
-                while (_running)
-                {
-                    std::unique_lock<std::mutex> lock(_mutex);
-                    _cv.wait(lock, [this]
-                             {
-                // Wake up if new data is available or if we are shutting down
-                return !_queue.empty() || !_running; });
+				_queue.pop();
+			}
+			_fileStream.flush();
+			_fileStream.close();
+		}
 
-                    // Process queue
-                    while (!_queue.empty())
-                    {
-                        const std::string &msg = _queue.front();
-                        std::cout << msg << '\n';
-                        _outputStream << msg << std::endl;
-                        _queue.pop();
-                    }
-                }
+		const bool logStdio = this->_logStdio.load();
+		std::string filepath;
+		std::ofstream _fileStream;
+		{
+			std::lock_guard<std::mutex> lock(_mutex);
+			filepath = _filepath;
+		}
+		if (!filepath.empty())
+		{
+			_fileStream = std::ofstream(filepath, std::ios::app);
+		}
 
-                // Flush anything remaining after _running becomes false
-                while (!_queue.empty())
-                {
-                    const auto &msg = _queue.front();
-                    std::cout << msg << std::endl;
-                    _outputStream << msg << std::endl;
-                    _queue.pop();
-                }
+		// Flush anything remaining after _running becomes false
+		while (!_queue.empty())
+		{
+			const auto& msg = _queue.front();
 
-                _outputStream.flush();
-                _outputStream.close();
-            }
-        };
+			if (logStdio)
+			{
+				std::cout << msg << std::endl;
+			}
 
-    }
+			if (!filepath.empty())
+			{
+				_fileStream << msg << std::endl;
+			}
+
+			_queue.pop();
+		}
+		_fileStream.flush();
+		_fileStream.close();
+	}
+};
+
 }
+}
+
 
 #endif // !WSTL_LOGGING_LOGGER
